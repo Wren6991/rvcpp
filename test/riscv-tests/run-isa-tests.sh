0774905159ae42cf91b2670f2bbc31aa116fd133
Luke Wren <wren6991@gmail.com>
2023-04-05 02:07:11 +0100

    Test script/environment bump, one p test is failing (rv32si-p-dirty) due to what seems to be a memory map assumption, otherwise all good with V/P tests

--- test/riscv-tests/run-isa-tests.sh
+++ test/riscv-tests/run-isa-tests.sh
@@ -6,6 +6,14 @@ make XLEN=32 clean
 
 # Note SKIP_V is something I added to their Makefile. We have no need for the
 # virtual memory test machine configuration.
-make -j$(nproc) XLEN=32 SKIP_V=1 rv32ui rv32uc rv32um rv32ua rv32mi rv32si
+make -j$(nproc) XLEN=32 rv32ui rv32uc rv32um rv32ua rv32mi rv32si
 
-for test in $(find -name "*-p-*.bin"); do echo $test; ../../../../rvcpp/rvcpp --bin $test --cycles 10000; done
+echo ""
+echo ">>> Running V environment tests"
+echo ""
+for test in $(find -name "*-v-*.bin"); do echo $test; ../../../../rvcpp/rvcpp --bin $test --cycles 100000 --cpuret; done
+
+echo ""
+echo ">>> Running P environment tests"
+echo ""
+for test in $(find -name "*-p-*.bin"); do echo $test; ../../../../rvcpp/rvcpp --bin $test --cycles 100000 --cpuret; done
