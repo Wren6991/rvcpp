0b6eb0246fde5dda0cb10b56a59b90a2f03d8e91
Luke Wren <wren6991@gmail.com>
2023-04-06 17:22:47 +0100

    Allow loading of multiple binaries. Allow tracing to become enabled only once a certain PC is reached.

--- rvcpp/main.cpp
+++ rvcpp/main.cpp
@@ -16,22 +16,24 @@
 // - M-mode and S-mode traps
 // - Sv32 virtual memory
 
+#define RAM_SIZE_DEFAULT (256 * 1024 * 1024)
 #define RAM_BASE         0x80000000u
-#define RAM_SIZE_DEFAULT (64 * 1024 * 1024)
 #define IO_BASE          0xe0000000u
-#define TBIO_BASE        IO_BASE
+#define TBIO_BASE        (IO_BASE + 0x0000)
 #define UART8250_BASE    (IO_BASE + 0x4000)
 #define MTIMER_BASE      (IO_BASE + 0x8000)
 
 const char *help_str =
-"Usage: rvcpp [--bin x.bin] [--dump start end] [--vcd x.vcd] [--cycles n] [--cpuret]\n"
-"    --bin x.bin      : Flat binary file loaded to address 0x0 in RAM\n"
+"Usage: rvcpp [--bin x.bin [@addr]] [--dump start end] [--cycles n] [--cpuret]\n"
+"    --bin x [@addr]  : Flat binary file loaded to absolute address addr.\n"
+"                       If no address is provided, load to the beginning of RAM.\n"
 "    --vcd x.vcd      : Dummy option for compatibility with CXXRTL tb\n"
 "    --dump start end : Print out memory contents between start and end (exclusive)\n"
 "                       after execution finishes. Can be passed multiple times.\n"
 "    --cycles n       : Maximum number of cycles to run before exiting.\n"
-"    --memsize n      : Memory size in units of 1024 bytes, default is 64 MB\n"
+"    --memsize n      : Memory size in units of 1024 bytes, default is 256 MB\n"
 "    --trace          : Print out execution tracing info\n"
+"    --trace-from pc  : Enable tracing upon reaching address pc\n"
 "    --cpuret         : Testbench's return code is the return code written to\n"
 "                       IO_EXIT by the CPU, or -1 if timed out.\n";
 
@@ -48,9 +50,11 @@ int main(int argc, char **argv) {
 	std::vector<std::tuple<uint32_t, uint32_t>> dump_ranges;
 	int64_t max_cycles = 100000;
 	uint32_t ram_size = RAM_SIZE_DEFAULT;
-	bool load_bin = false;
-	std::string bin_path;
+	std::vector<std::string> bin_paths;
+	std::vector<ux_t> bin_addrs;
 	bool trace_execution = false;
+	bool trace_from_pc = false;
+	ux_t trace_from_pc_val = 0;
 	bool propagate_return_code = false;
 
 	for (int i = 1; i < argc; ++i) {
@@ -58,17 +62,20 @@ int main(int argc, char **argv) {
 		if (s == "--bin") {
 			if (argc - i < 2)
 				exit_help("Option --bin requires an argument\n");
-			load_bin = true;
-			bin_path = argv[i + 1];
+			bin_paths.push_back(argv[i + 1]);
 			i += 1;
-		}
-		else if (s == "--vcd") {
+			if (argc - i >= 2 && argv[i + 1][0] == '@') {
+				bin_addrs.push_back(std::stoul(&argv[i + 1][1], 0, 0));
+				i += 1;
+			} else {
+				bin_addrs.push_back(RAM_BASE);
+			}
+		} else if (s == "--vcd") {
 			if (argc - i < 2)
 				exit_help("Option --vcd requires an argument\n");
-			// (We ignore this argument, it's supported for
+			// (We ignore this argument, it's supported for compatibility with Hazard3 tb.
 			i += 1;
-		}
-		else if (s == "--dump") {
+		} else if (s == "--dump") {
 			if (argc - i < 3)
 				exit_help("Option --dump requires 2 arguments\n");
 			dump_ranges.push_back(std::make_tuple(
@@ -76,26 +83,27 @@ int main(int argc, char **argv) {
 				std::stoul(argv[i + 2], 0, 0)
 			));
 			i += 2;
-		}
-		else if (s == "--cycles") {
+		} else if (s == "--cycles") {
 			if (argc - i < 2)
 				exit_help("Option --cycles requires an argument\n");
 			max_cycles = std::stol(argv[i + 1], 0, 0);
 			i += 1;
-		}
-		else if (s == "--memsize") {
+		} else if (s == "--memsize") {
 			if (argc - i < 2)
 				exit_help("Option --memsize requires an argument\n");
 			ram_size = 1024 * std::stol(argv[i + 1], 0, 0);
 			i += 1;
-		}
-		else if (s == "--trace") {
+		} else if (s == "--trace") {
 			trace_execution = true;
-		}
-		else if (s == "--cpuret") {
+		} else if (s == "--trace-from") {
+			trace_from_pc = true;
+			if (argc - i < 2)
+				exit_help("Option --trace-from requires an argument\n");
+			trace_from_pc_val = std::stol(argv[i + 1], 0, 0);
+			i += 1;
+		} else if (s == "--cpuret") {
 			propagate_return_code = true;
-		}
-		else {
+		} else {
 			fprintf(stderr, "Unrecognised argument %s\n", s.c_str());
 			exit_help("");
 		}
@@ -113,25 +121,34 @@ int main(int argc, char **argv) {
 
 	RVCore core(mem, RAM_BASE, RAM_BASE, ram_size);
 
-	if (load_bin) {
-		std::ifstream fd(bin_path, std::ios::binary | std::ios::ate);
+	for (size_t i = 0; i < bin_paths.size(); ++i) {
+		if (trace_execution) {
+			printf("Loading file \"%s\" at %08x\n", bin_paths[i].c_str(), bin_addrs[i]);
+		}
+		std::ifstream fd(bin_paths[i], std::ios::binary | std::ios::ate);
 		std::streamsize bin_size = fd.tellg();
-		if (bin_size > ram_size) {
-			fprintf(stderr, "Binary file (%ld bytes) is larger than memory (%u bytes)\n", bin_size, ram_size);
+		if (bin_size + bin_addrs[i] - RAM_BASE > ram_size) {
+			fprintf(stderr, "Binary file (%ld bytes) loaded to %08x extends past end of memory (%08x through %08x)\n", bin_size, bin_addrs[i], RAM_BASE, RAM_BASE + ram_size - 1);
+			return -1;
+		} else if (bin_addrs[i] < RAM_BASE) {
+			fprintf(stderr, "Binary file load address %08x is less than RAM base address %08x\n", bin_addrs[i], RAM_BASE);
 			return -1;
 		}
 		fd.seekg(0, std::ios::beg);
-		fd.read((char*)core.ram, bin_size);
+		fd.read((char*)&core.ram[bin_addrs[i] - RAM_BASE], bin_size);
 	}
 
 	int64_t cyc;
 	int rc = 0;
 	try {
-		for (cyc = 0; cyc < max_cycles; ++cyc) {
+		for (cyc = 0; cyc < max_cycles || max_cycles == 0; ++cyc) {
 			core.step(trace_execution);
 			if (!(cyc & 0xfff)) {
 				mtimer.step_time();
 				core.csr.set_irq_t(mtimer.irq_status(0));
+			}
+			if (!trace_execution && trace_from_pc && core.pc == trace_from_pc_val) {
+				trace_execution = true;
 			}
 		}
 		if (cyc == max_cycles) {
